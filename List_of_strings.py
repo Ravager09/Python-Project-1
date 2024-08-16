1d92ceb535ac02299cae7dcf83053b8840294459
Adarsh Mishra <pg23adarsh.mishra@isbmb.ac.in>
2024-08-16 12:40:14 +0530

    first_files

---
+++ List_of_strings.py
@@ -0,0 +1,36 @@
+from print_statement import repeat_hello, hello_name
+
+
+
+def print_even_up_to_number(input_number):
+    list_of_numbers = list(range(1, input_number+1))
+    print(list_of_numbers)
+    for num in list_of_numbers:
+        if num % 2 == 0:
+            print(num)
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print("Hello World")
+    repeat_hello(someone="Adarsh", n_times=5)
+    hello_name("Adarsh")
+
+
+    list_of_names = ["Adarsh", "Anish", "Rishabh"]
+    for name in list_of_names:
+        hello_name(name)
+
+
+    number_list = [1, 2, 3, 4, 5]
+    print(number_list)
+
+    for num in number_list:
+        print(num)
+
+    print_even_up_to_number(20)
